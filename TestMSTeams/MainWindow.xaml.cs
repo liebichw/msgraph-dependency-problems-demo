100de0abd81225c32115bd45ae5cdf05e8b56cb5
Wolfgang Liebich <Wolfgang.Liebich@atos.net>
2024-04-18 09:57:03 +0200

    better error reporting for...
    ...reflection errors

--- TestMSTeams/MainWindow.xaml.cs
+++ TestMSTeams/MainWindow.xaml.cs
@@ -17,22 +17,52 @@ namespace TestMSTeams
 
     public MainWindow()
     {
-      var tenantId = RequireEnvVar("TENANT_ID");
-      var clientId = RequireEnvVar("CLIENT_ID");
-      var userId = Environment.GetEnvironmentVariable("USER_ID");
-      var useProxy = Environment.GetEnvironmentVariable("USE_PROXY") == "true";
-      var redirectUrl = Environment.GetEnvironmentVariable("REDIRECT_URL") ?? "http://localhost";
-      var configData = new ConfigData(tenantId, clientId, userId, useProxy, redirectUrl);
+      ConfigData configData;
+      try
+      {
+        var tenantId = RequireEnvVar("TENANT_ID");
+        var clientId = RequireEnvVar("CLIENT_ID");
+        var userId = Environment.GetEnvironmentVariable("USER_ID");
+        var useProxy = Environment.GetEnvironmentVariable("USE_PROXY") == "true";
+        var redirectUrl = Environment.GetEnvironmentVariable("REDIRECT_URL") ?? "http://localhost";
+        configData = new ConfigData(tenantId, clientId, userId, useProxy, redirectUrl);
+      }
+      catch (Exception e)
+      {
+        MessageBox.Show("Exception reading data: " + e.Message);
+        throw;
+      }
 
-      var pluginPath = RequireEnvVar("PLUGIN_PATH");
-      var pluginFactoryName = RequireEnvVar("PLUGIN_FACTORY");
+      var progress = "";
+      try
+      {
+        var pluginPath = RequireEnvVar("PLUGIN_PATH");
+        progress += $"Did set pluginPath={pluginPath}:";
+
+        var pluginFactoryName = RequireEnvVar("PLUGIN_FACTORY");
+        progress += $"Will use pluginFactory={pluginFactoryName}:";
+
+        var assembly = Assembly.LoadFile(pluginPath) ?? throw new ArgumentException("Cannot load assembly " + pluginPath);
+        progress += $"Did load assembly {assembly}";
+
+        var rawInstance = assembly.CreateInstance(pluginFactoryName);
+        progress += $"created rawInstance={rawInstance}:";
+        if (rawInstance == null)
+        {
+          throw new ArgumentException("Could not create raw plugin factory instance of type " + pluginFactoryName);
+        }
+
+        var factory = (IPluginFactory)rawInstance ?? throw new ArgumentException("Cannot cast  " + rawInstance+" to " + pluginFactoryName + " loaded from " + assembly);
+        _plugin = factory.CreatePlugin(configData);
+        if (_plugin == null) throw new Exception("Factory " + factory + " did not create a plugin");
+      }
+      catch (Exception e)
+      {
+        MessageBox.Show("Exception loading plugin: progress="+progress+": Exception=" + e.Message);
+        throw;
+      }
 
 
-      if (pluginFactoryName == null) throw new ArgumentNullException(nameof(pluginFactoryName));
-      var assembly = Assembly.LoadFile(pluginPath) ?? throw new ArgumentException("Canot load assembly "+pluginPath);
-
-      IPluginFactory factory = (IPluginFactory)assembly.CreateInstance(pluginFactoryName) ?? throw new ArgumentException("Cannot create "+pluginFactoryName+" instance from "+assembly);
-      _plugin = factory.CreatePlugin(configData);
       InitializeComponent();
     }
 
